2f29211a12d3e60d1748dbe6972611b25fd59402
YoawJC <yoawjc@gmail.com>
2021-08-11 13:52:16 +0800

    teddt

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Counter />
+                <FeeCalculator />
             </header>
         </div>
     );

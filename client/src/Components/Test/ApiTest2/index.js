6ee2f3b0f4aef76bbacea6f76c2c12bcbf8a7a77
Nicholas Choong <nicholascwc1997@gmail.com>
2021-08-24 18:15:11 +0800

    Moved to feature experiment folder

--- client/src/Components/Test/ApiTest2/index.js
+++ client/src/Components/Test/ApiTest2/index.js
@@ -6,15 +6,20 @@ const ApiTest2 = () => {
   const [data, setData] = useState('')
   const [majorList, setMajorList] = useState([])
   const [request, response, loading, error] = useFetch()
-  const [selection, setSelection] = useState({ value: '', label: '' })
+  const [selection, setSelection] = useState({
+    value: '',
+    label: ''
+  })
 
   const focusRef = useRef()
+  const node = focusRef.current
 
   const loadInitialData = useCallback(async () => {
     const initialData = await request.post('/Calculator/GetCourses', {
       feeCategory: 'INTUG',
       feeYear: '2021'
     })
+    if (error) console.error(error)
     if (response.ok) {
       setData(JSON.stringify(initialData[0], null, 2))
       const newMajorList = Object.entries(initialData[0]).map(obj => ({
@@ -23,11 +28,10 @@ const ApiTest2 = () => {
       }))
       setMajorList(newMajorList)
     }
-  }, [request, response])
+  }, [request, response, error])
 
   useEffect(() => {
-    const node = focusRef.current
-    node.focus()
+    node?.focus()
   })
 
   useEffect(() => {
@@ -36,7 +40,13 @@ const ApiTest2 = () => {
 
   const changeHandler = event => {
     setSelection({ value: event?.value, label: event?.label })
+    // console.log(event?.value)
+    console.log(node.select.value)
   }
+
+  //   const openHandler = () => {
+  //     setSelection({ value: '', label: '' })
+  //   }
 
   const submitHandler = event => {
     event.preventDefault()
@@ -48,14 +58,19 @@ const ApiTest2 = () => {
       {error && 'Error!'}
       {loading && 'Loading...'}
       <form onSubmit={submitHandler}>
-        <Select
-          options={majorList}
-          isClearable={true}
-          placeholder='Choose a Major'
-          onChange={changeHandler}
-          ref={focusRef}
-        />
-        <button type='submit'>Submit</button>
+        <div style={{ maxWidth: '600px' }}>
+          <Select
+            options={majorList}
+            isClearable={true}
+            placeholder='Choose a Major'
+            onChange={changeHandler}
+            // onMenuOpen={openHandler}
+            ref={focusRef}
+          />
+          <button disabled={!selection.value} type='submit'>
+            Submit
+          </button>
+        </div>
       </form>
       <code>
         <pre>{data}</pre>

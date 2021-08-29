98b70e1b27317de205e484d6d1db9b100a3534cf
Nicholas Choong <nicholascwc1997@gmail.com>
2021-08-29 15:53:06 +0800

    Removed unused components

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,18 +1,12 @@
 import React from 'react'
-// import FeeTextField from '../Components/FeeTextField'
-// import ApiTest2 from '../Components/Test/ApiTest2'
 import FeeCalculator from '../Components/FeeCalculator'
-import Buttons from '../Components/Test/Buttons'
-import ButtonsReact from '../Components/Test/ButtonsReact'
 // import './index.css'
 
 const App = () => {
   return (
     <>
-      <Buttons />
-      <br />
-      <ButtonsReact />
       <div className=''>
+        <h1>UWA Fee Calculator</h1>
         <FeeCalculator />
       </div>
     </>

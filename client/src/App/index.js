bd5dd11a9368a5d0fa38091f934cb4ea4c0fb400
Nicholas Choong <nicholascwc1997@gmail.com>
2021-08-30 23:58:06 +0800

    A complete page but some components are empty

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
 import NavBar from '../components/NavBar'
 import Banner from '../components/Banner'
 import Title from '../components/Title'
 import FeeCalculator from '../components/FeeCalculator'
+import Footer from '../components/Footer'
 
 const App = () => {
   return (
     <>
-      <div className=''>
-        <NavBar />
-        <Banner />
+      <NavBar />
+      <Banner />
+      <Container as='main' className=''>
         <Title />
         <FeeCalculator />
-      </div>
+      </Container>
+      <Footer />
     </>
   )
 }

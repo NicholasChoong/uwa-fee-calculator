1b0ee5960d5f7de4c15b368b3780d1059d78a9a0
Nicholas Choong <nicholascwc1997@gmail.com>
2021-08-30 19:39:14 +0800

    Imports video on load

--- client/src/Components/Banner/index.js
+++ client/src/Components/Banner/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import video from '../../assets/videos/banner-video.mp4'
 
 const Banner = () => {
   return (
@@ -9,11 +10,8 @@ const Banner = () => {
 
         <div className='video-container'>
           <div className='color-overlay'></div>
-          <video autoPlay loop muted>
-            <source
-              src='../../assets/videos/banner-video.mp4'
-              type='video/mp4'
-            />
+          <video autoPlay={true} loop muted>
+            <source src={video} type='video/mp4' />
           </video>
         </div>
       </div>

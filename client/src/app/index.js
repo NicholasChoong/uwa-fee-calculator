507c56b29e7211590ca6332ee69893a14d7c21d0
Nicholas Choong <nicholascwc1997@gmail.com>
2021-10-08 13:21:51 +0800

    Add dynamic import for page

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Banner from '../components/Banner'
-import FeeCalculator from '../components/FeeCalculator'
 import Footer from '../components/Footer'
+
+const FeeCalculator = lazy(() => import('../components/FeeCalculator'))
 
 const App = () => {
   return (
-    <>
+    <Suspense
+      fallback={
+        <>
+          <NavBar />
+          <Banner />
+          <main>
+            <div style={{ textAlign: 'center' }}>
+              <br />
+              <br />
+              <br />
+              <br />
+              <hr style={{ width: '50%', margin: 'auto' }} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </main>
+          <Footer />
+        </>
+      }
+    >
       <Router>
         <Switch>
           <Route exact path='/'>
@@ -20,7 +54,7 @@ const App = () => {
           </Route>
         </Switch>
       </Router>
-    </>
+    </Suspense>
   )
 }
 

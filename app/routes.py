9d43970edd6106295a96f85ca4a982825158cf10
jcwl22 <jeremy.leaversuch@gmail.com>
2021-10-08 16:52:03 +0800

    Updated more api calls
    getCourseFee and getUnitInfo now using the database.

--- app/routes.py
+++ app/routes.py
@@ -57,20 +57,89 @@ def getCourses():
 def getCourseFee():
     if not request.json or not "feeCategory" in request.json:
         abort(400)
-    result = requests.post(
-        url="https://www.fees.uwa.edu.au/Calculator/GetCourseFee", data=request.json
-    )
-    return jsonify(result.json()), 200
+
+    stype = request.json["feeCategory"]
+    feeYear = request.json["feeYear"]
+    course = request.json["courseCode"]
+    startYear = request.json["year"]
+
+    #startYear either 2020, 2021, 2021 on, Pre-2021
+    # remove "Starting " from startYear
+    startYear = startYear[9:]
+
+    if stype == "INTUG" or stype == "INTPG" or stype == "INTHDR":
+        courseInfo = international.query.filter_by(
+            course_code=course, start_year=startYear
+        ).first()
+        feePoint = courseInfo.total_fee / courseInfo.total_points
+    elif stype == "DPG":
+        courseInfo = domesticPost.query.filter_by(
+            course_code=course, start_year=startYear
+        ).first()
+        feePoint = courseInfo.fee_per_point
+    # Not in database
+    else:
+        result = requests.post(
+            url="https://www.fees.uwa.edu.au/Calculator/GetCourseFee", data=request.json
+        )
+        return jsonify(result.json()), 200
+
+    result = [
+        {
+            "course_name": courseInfo.course_title,
+            "course_credit_point": courseInfo.total_points,
+            "annual_credit_point": courseInfo.yearly_points,
+            "fee_per_credit_point": round(feePoint, 2),
+            "fee": round(feePoint * courseInfo.yearly_points, 2),
+            "total_fee": courseInfo.total_fee,
+        }
+    ]
+
+    return jsonify(result), 200
+
 
 
 @app.route("/Calculator/GetUnitInfo", methods=["POST"])
 def getUnitInfo():
     # if not request.json or not "feeCategory" in request.json:
     #     abort(400)
+    """
     result = requests.post(
         url="https://www.fees.uwa.edu.au/Calculator/GetUnitInfo", data=request.json
     )
     return jsonify(result.json()), 200
+    """
+
+    stype = request.json['feeCategory']
+    feeYear = request.json['feeYear']
+    unit = request.json['unit']
+    course = request.json['courseCode']
+    startYear = request.json['year']
+
+    #startYear either 2020, 2021, 2021 on, Pre-2021
+    # remove "Starting " from startYear
+    startYear = startYear[9:]
+
+    unit.split('[')
+    unitTitle = unit[0]
+    unitCode = unit[1][:-1]
+
+    pointInfo = units.query.filter_by(unit_code=unitCode, unit_title=unitTitle).all()
+
+    if stype == 'INTUG' or stype == 'INTPG' or stype == 'INTHDR':
+        clust = pointInfo.int_clust
+    elif stype == 'DNA':
+        clust = pointInfo.non_clust
+    else:
+        clust = pointInfo.dom_clust
+
+    clustInfo = cluster.query.filter_by(cluster=clust, year=startYear)
+
+    result = [{"creditpoint": pointInfo.creditPoint,
+               "eftsl": pointInfo.creditPoints/48,
+               "fee": clustInfo.fee}]
+    
+    return jsonify(result)
 
 
 @app.route("/Calculator/GetUnitsForMajor", methods=["POST"])

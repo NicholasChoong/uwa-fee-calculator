2037457376436e3dd1e1a2604ff6fc86bd5fa5d6
Nicholas Choong <nicholascwc1997@gmail.com>
2021-10-12 22:59:15 +0800

    Added a new API

--- client/src/components/FeeCalculator/FeeCategoryAndYear/index.js
+++ client/src/components/FeeCalculator/FeeCategoryAndYear/index.js
@@ -8,7 +8,7 @@ import feeYearsOptions from '../../../libs/feeYears.json'
 const currentYear = new Date().getFullYear()
 
 const FeeCategoryAndYear = props => {
-  const { updateData, updateCourseListAndYearList, updatePage, page } = props
+  const { updateData, updateCourseList, updatePage, page } = props
   const [request, response, loading, error] = useFetch()
   const [selection, setSelection] = useState({
     feeCategory: '',
@@ -21,28 +21,16 @@ const FeeCategoryAndYear = props => {
     if (error) console.error(error)
     if (response.ok) {
       const courses = initialData[0]
-      const years = initialData[1]
       const newCourseList = Object.entries(courses).map(
         ([courseCode, courseName]) => ({
           value: courseCode,
           label: courseName
         })
       )
-      const newYearList = years.map(year => ({
-        value: year,
-        label: `Starting ${year}`
-      }))
-      updateCourseListAndYearList(newCourseList, newYearList)
+      updateCourseList(newCourseList)
       updatePage(PAGES.COURSE_AND_YEAR)
     }
-  }, [
-    request,
-    response,
-    error,
-    selection,
-    updateCourseListAndYearList,
-    updatePage
-  ])
+  }, [request, response, error, selection, updateCourseList, updatePage])
 
   const changeCategoryHandler = event => {
     setSelection(prev => ({
